762ad37aa98cadefe034c576bbc64a63e20c2b7d
Anjoli Owens <107882438+aowens35@users.noreply.github.com>
2023-02-15 08:03:10 +0000

    /case about h1 elements

--- server.js
+++ server.js
@@ -7,17 +7,19 @@ const serv = http.createServer((req, res)=> {
     res.setHeader('Content-Type', 'text/html');
 
     let path = './views/';
-
     switch(req.url){
         case '/':
-            path+= 'index.html';
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html'
             res.statusCode = 200;
             break;
         case '/about-me':
-            path += 'about.html';
             res.statusCode = 301;
             res.setHeader('Location', '/about');
-            res.end();
+            res.end(data);
             break;
         default:
             path += '404.html';
@@ -31,7 +33,7 @@ const serv = http.createServer((req, res)=> {
             res.end();
         }else {
             //res.write(data);
-            res.end();
+            res.end(data);
         }
     })
 
